bf6a13ca5155661e57d217100639bc79a069d295
Onat Dibo <82000752+main-hesflay@users.noreply.github.com>
2022-04-11 04:35:46 -0700

    Update main.js

--- Tutorials-Javascript/main.js
+++ Tutorials-Javascript/main.js
@@ -27,14 +27,74 @@ function getPerimeter(length, width) {
 
   // ********** Day 1: Functions ********** //
 
+function factorial(n) {
+    let finalVal = 1;
+    for (let i=n; i > 1; i--) {
+        finalVal *= i
+    }
+    
+    return finalVal;
+}
+
+
+function main() {
+    const n = +(readLine());
+    
+    console.log(factorial(n));
+}
+
   
  // ********** Day 2: Conditional Statements: Switch ********** //
 
 
 // ********** Day 2: Loops ********** //
 
+function vowelsAndConsonants(s) {
+    for(let i= 0; i < s.length; i++) {
+        if(s.charAt(i).match(/[aeiou]/)) {
+            console.log(s.charAt(i));
+        }
+    }
+    
+    // consonants
+    for(let i= 0; i < s.length; i++) {
+        if(s.charAt(i).match(/[^aeiou]/)) {
+            console.log(s.charAt(i));
+        }
+    }
+}
+
+
+function main() {
+    const s = readLine();
+    
+    vowelsAndConsonants(s);
+}
+
 
 // ********** Day 3: Intro to Conditional Statements ********** //
 
 
 // ********** Day 3: Arrays ********** //
+
+function getSecondLargest(nums) {
+    // Complete the function
+    nums = new Set(nums);
+    nums = Array.from(nums)
+    nums = nums.sort(comparator)
+    return nums [nums.length - 2]
+    
+    function comparator(a, b) {
+        return a - b;
+    }
+}
+
+
+function main() {
+    const n = +(readLine());
+    const nums = readLine().split(' ').map(Number);
+    
+    console.log(getSecondLargest(nums));
+}
+
+// FINISH...
